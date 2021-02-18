5da09f567071517117644645a4eac00268c6bbfd
Marco Cotugno <mcotugno10@gmail.com>
2021-02-18 13:56:21 +0100

    added right widgets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Feed from './components/Feed';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Widgets from './components/Widgets';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div className="app__body">
           <Sidebar />
           <Feed />
+          <Widgets />
         </div>
     </div>
   );
